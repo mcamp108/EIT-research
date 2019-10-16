4278bb4a046934c822c61b20e13c232d9884fe1c
mcamp108 <mark.c91@gmail.com>
2019-10-16 12:29:55 -0400

    updates

--- Other/make_fun.m
+++ Other/make_fun.m
@@ -2,11 +2,10 @@ function [] = make_fun(V)
 
 fun_name = horzcat('function out= ', V, '(in)');
 end_function= 'end % end function';
-str_date=horzcat('%   ', datestr(date,'dd.mmm.yyyy'));
 fid = fopen('C:\Users\Mark\Documents\GraduateStudies\LAB\Scripts\EIT-reserach\Other\my_template.m');
 F = fread(fid, '*char')';
 fclose(fid);
-text= [fun_name newline F newline str_date newline newline newline end_function];
+text= [fun_name newline F newline newline newline end_function];
 fid = fopen([V '.m'], 'w');
 fwrite(fid, text);
 fclose(fid);
